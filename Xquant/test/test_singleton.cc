7f272c34a784ca337f8d2ad85e9314e7d7f8ae4d
ifquant@163.com <ifquant@163.com>
2018-11-07 03:33:51 +0800

    add test

---
+++ Xquant/test/test_singleton.cc
@@ -0,0 +1,22 @@
+
+/* ====================================================
+#   Copyright (C)2018 All rights reserved.
+#
+#   Author        : IfQuant
+#   Email         : ifquant@163.com
+#   File Name     : test_singleton.cc
+#   Last Modified : 2018-11-07 03:22
+#   Describe      :
+#
+# ====================================================*/
+
+#include <iostream>
+#include "xquant/common.h"
+using namespace std;
+
+
+int main(int argc, char *argv)
+{
+  std::cout << " test_singleton" ;
+  return 0;
+}
